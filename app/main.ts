e7bffa028dbb88c3aa6394312c72e5263e748f2d
Evan Sosenko <razorx@evansosenko.com>
2016-07-22 17:36:37 -0700

    Enable production mode in production

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,12 @@
+import { enableProdMode } from '@angular/core'
 import { bootstrap } from '@angular/platform-browser-dynamic'
+
 import { AppComponent } from './components/app'
 
 export default () => {
+  if (process.env.NODE_ENV === 'production') {
+    enableProdMode()
+  }
+
   bootstrap(AppComponent)
 }
